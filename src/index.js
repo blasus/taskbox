8a389ead4269ca772133afa91008bdcb876b1a6f
Francesco <xblasi@hotmail.it>
2020-06-21 17:20:16 +0100

    added react fontAwesome package

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faEdit, faPlus } from '@fortawesome/free-solid-svg-icons';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+library.add(fab, faEdit, faPlus);
 
 ReactDOM.render(
   <React.StrictMode>
